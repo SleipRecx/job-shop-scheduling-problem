4afb819252a613e1cd46dd27fdc1d388c87a56b0
Espen Meidell <espen.meidell@gmail.com>
2018-04-25 10:51:13 +0200

    add neighbourhood list

--- graph/Graph.go
+++ graph/Graph.go
@@ -10,36 +10,36 @@ type Node struct {
 }
 
 type ConjunctiveArc struct {
-	From, To *Node
+	From, To Node
 	Time     int
 }
 
 type DisjunctiveArc struct {
-	From, To *Node
+	From, To Node
 	Time     int
 }
 
 func MakeGraph(problemFormulation io.ProblemFormulation) {
 	source := Node{-1, -1, 0}
 	sink := Node{-1, -1, 0}
-	nodes := []*Node{&source}
+	nodes := []Node{source}
 	conjunctiveArcs := make([]ConjunctiveArc, 0)
 	disjunctiveArcs := make([]DisjunctiveArc, 0)
-	machineToNodesMap := make(map[int][]*Node)
+	machineToNodesMap := make(map[int][]Node)
 
 	// Create conjunctive arcs (technological order)
 	for jobId, requirements := range problemFormulation.Sequences {
 		previous := source
 		for _, requirement := range requirements {
 			node := Node{jobId, requirement.Machine, requirement.Time}
-			machineToNodesMap[requirement.Machine] = append(machineToNodesMap[requirement.Machine], &node)
-			conjunctiveArcs = append(conjunctiveArcs, ConjunctiveArc{&previous, &node, previous.Time})
-			nodes = append(nodes, &node)
+			machineToNodesMap[requirement.Machine] = append(machineToNodesMap[requirement.Machine], node)
+			conjunctiveArcs = append(conjunctiveArcs, ConjunctiveArc{previous, node, previous.Time})
+			nodes = append(nodes, node)
 			previous = node
 		}
-		conjunctiveArcs = append(conjunctiveArcs, ConjunctiveArc{&previous, &sink, previous.Time})
+		conjunctiveArcs = append(conjunctiveArcs, ConjunctiveArc{previous, sink, previous.Time})
 	}
-	nodes = append(nodes, &sink)
+	nodes = append(nodes, sink)
 
 	// Create disjunctive arcs (jobs belonging to machines)
 	for _, nodePtrs := range machineToNodesMap {
@@ -52,5 +52,11 @@ func MakeGraph(problemFormulation io.ProblemFormulation) {
 		}
 	}
 
-	fmt.Println(len(disjunctiveArcs))
+	neighbours := make(map[Node][]Node)
+	for _, edge := range conjunctiveArcs {
+		neighbours[edge.From] = append(neighbours[edge.From], edge.To)
+	}
+	for _, edge := range disjunctiveArcs {
+		neighbours[edge.From] = append(neighbours[edge.From], edge.To)
+	}
 }
